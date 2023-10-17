f28e1d9a1b11d827e9391b72c6ec33c5f366d2a9
danylokotur <kotur.danylo@gmail.com>
2023-10-17 11:28:34 +0300

    h

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,8 +1,8 @@
 import {User} from "./User";
 
-const {Users} = () => {
+const Users = () => {
     const users =[
-        {id: 1, name:'Гомер', image:'https://static.wikia.nocookie.net/p__/images/3/3c/HomerSimpson.png'},
+        {id: 1, name:'Гомер', image:'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'},
         {id: 2, name:'Барт', image:'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'},
         {id: 3, name:'Мардж', image:'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'},
         {id: 4, name:'Ліза', image:'https://static.wikia.nocookie.net/simpsons/images/5/57/Lisa_Simpson2.png'},
@@ -11,8 +11,7 @@ const {Users} = () => {
     ]
     return (
         <div>
-            {users.map(user=> <User key={user.id} user={user}/>)}
-
+            {users.map(user=><User key={user.id} user={user}/>)}
         </div>
     );
 };
