d84aaa1d4ebd798ed243395db8b21dea2800ef65
danylokotur <kotur.danylo@gmail.com>
2023-10-26 23:31:31 +0300

    hw4

---
+++ src/pages/PostsPage.js
@@ -0,0 +1,11 @@
+import {Posts} from "../components/PostContainer/Posts";
+
+const PostsPage = () => {
+    return (
+        <div>
+            <Posts/>
+        </div>
+    );
+};
+
+export {PostsPage};
